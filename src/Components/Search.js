cbbca4be7a5bd6f95cf3a84562c24decbd25aeed
Gary Barkauski <gbarka2@gmail.com>
2021-01-16 12:49:14 -0700

    Build search frame

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Search = (props) => {
   return (
-    <div>Search Component</div>
+    <div>Search Component
+      <input type="text" name="city" placeholder="Enter City Name"/>
+      <input type="submit"/>
+    </div>
   )
 }
 
