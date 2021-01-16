55a304e9be28ce9bfbbeed02ee870a11031e50ee
Gary Barkauski <gbarka2@gmail.com>
2021-01-16 13:52:28 -0700

    Complete Airport card initial styling

--- src/Components/Airport.js
+++ src/Components/Airport.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Airport = (props) => {
   return (
-    <div>Airport Component</div>
+    <div className="airport-card">
+      <h3>codeIataAirport</h3>
+      <h4>nameAirport</h4>
+      <p>nameCountry</p>
+      <div className="favorites-bar">
+        <p>timezone</p>
+        <p>Favorite Icon</p>
+      </div>
+    </div>
   )
 }
 
