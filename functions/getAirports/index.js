c2c6b45040d54a9b143e14224dc2e80c1df24fad
Gary Barkauski <gbarka2@gmail.com>
2021-02-12 12:08:22 -0700

    Add apikey

--- functions/getAirports/index.js
+++ functions/getAirports/index.js
@@ -1,9 +1,25 @@
+const fetch = require('node-fetch')
+
 //We export the function
 exports.handler = async function(event, context) {
   //Fetch request details from event object
   const {path, httpMethod, headers, queryStringParameters, body} = event
   const city = queryStringParameters.city
-  const myBody = {city}
+
+
+  const apiKey = process.env.apiKey
+
+  const url = `https://aviation-edge.com/v2/public/autocomplete?key=${apiKey}&city=${city}`
+
+  const getCity = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    data.airportByCities = []
+    setData(data)
+  }
+
+  const myBody = {city, apiKey}
+
   // return some JSON data with a status of 200
   return {
     statusCode: 200,
